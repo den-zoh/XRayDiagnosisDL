f78132d4b8084209b77897d359cc21938514606c
PrivateIce <dejahvu.too@gmail.com>
2024-11-10 18:25:44 -0500

    Implemented the thumbnail selection and loading of the zoomed image.

--- visualization/scripts.js
+++ visualization/scripts.js
@@ -13,6 +13,8 @@ const rightColumn = d3.select("#right-column");
 const rightColumnWidth = document.querySelector(".right").offsetWidth;
 const rightColumnHeight = document.querySelector(".right").offsetHeight;
 
+const confusionWidth = rightColumnWidth; 
+const confusionHeight = rightColumnHeight / 2;
 
 
 const xraybuttons = leftColumn.select("#button-container")
@@ -43,18 +45,18 @@ xraybuttons.append("path")
         h -${width - radius}
         z
     `)
-    .style("fill", "#69b3a2")
-    .style("stroke", "#333")
-    .style("stroke-width", 2);
+    .style("fill", "#3edffd")
+    .style("stroke", "#16afeb")
+    .style("stroke-width", 3);
 
 xraybuttons.append("rect")
 	.attr("x", margin + width)
     .attr("y", 5)
     .attr("width", width)
     .attr("height", height/2)
-    .style("fill", "#69b3a2")
-    .style("stroke", "#333")
-    .style("stroke-width", 2);
+    .style("fill", "#3edffd")
+    .style("stroke", "#16afeb")
+    .style("stroke-width", 3);
 
 xraybuttons.append("path")
     .attr("d", `
@@ -65,16 +67,15 @@ xraybuttons.append("path")
         h ${width - radius}
         z
     `)
-    .style("fill", "#69b3a2")
-    .style("stroke", "#333")
-    .style("stroke-width", 2);
+    .style("fill", "#3edffd")
+    .style("stroke", "#16afeb")
+    .style("stroke-width", 3);
 
     
     
 const confusion = d3.select("#confusion")
-    .attr("viewBox", "0 0 400 200")
+    .attr("viewBox", "0 0 785 624")
     .attr("preserveAspectRatio", "xMidYMid meet")
-    .style("background-color", "#ccc")
     .attr("width", "100%")
     .attr("height", "50%");
 
@@ -82,20 +83,69 @@ const tnContainer = rightColumn.append("div").classed("thumbnail-container", tru
 
 const thumbnail1 = tnContainer.append("svg")
     .classed("thumbnail", true)
-    .attr("viewBox", "0 0 400 200");
+    .attr("viewBox", "0 0 400 200")
+	.style("background-color", "#ccc");
 
 const thumbnail2 = tnContainer.append("svg")
+	.attr("id", "Confusion Matrix")
     .classed("thumbnail", true)
-    .attr("viewBox", "0 0 400 200");
-
-
+    .classed("thumbnail-border", true)
+    .attr("viewBox", "0 0 400 200")
+    .append("image")
+    .attr("href", "images/confusion_matrix.png")
+    .attr("x", 2 * (785 - 200)/3)
+    .attr("y", 0)
+    .attr("width", 785)
+    .attr("height", 624)
+    .style("transform", `scale(${200 / 785}, ${200 / 624})`);
+    
 const thumbnail3 = tnContainer.append("svg")
     .classed("thumbnail", true)
-    .attr("viewBox", "0 0 400 200");
+    .attr("viewBox", "0 0 400 200")
+	.style("background-color", "#ccc");
 
 const thumbnail4 = tnContainer.append("svg")
     .classed("thumbnail", true)
-    .attr("viewBox", "0 0 400 200");
+    .attr("viewBox", "0 0 400 200")
+	.style("background-color", "#ccc");
+
+d3.selectAll(".thumbnail")
+    .on("click", function() {
+    	confusion.selectAll(".dynamic-image").remove();
+        confusion.selectAll(".dynamic-element").remove();
+    	d3.selectAll(".thumbnail").classed("thumbnail-border", false);
+        d3.select(this).classed("thumbnail-border", true);
+		
+		const thumbnail = d3.select(this);
+        const image = thumbnail.select("image").node();
+		const width = +image.getAttribute("width");
+		const height = +image.getAttribute("height");	
+	
+		confusion
+			.attr("viewBox", `0 0 ${width} ${height}`)
+			.attr("width", width)
+			.attr("height", height);
+		
+		if (image) {
+			const clonedImage = image.cloneNode(true);
+			d3.select(clonedImage).classed("dynamic-image", true);
+			confusion.node().appendChild(clonedImage);
+			
+			d3.select(clonedImage)
+				.classed("dynamic-image", true)
+				.attr("x", 0)
+				.attr("y", 0)
+				.attr("width", width)
+				.attr("height", height)
+				.style("transform", "none");			
+		} else {
+			thumbnail.selectAll("*").each(function() {
+				const element = this.cloneNode(true);
+				d3.select(element).classed("dynamic-element", true);
+				confusion.node().appendChild(element);
+			});
+		}
+    });
     
     
 // Placeholder for additional D3.js code
